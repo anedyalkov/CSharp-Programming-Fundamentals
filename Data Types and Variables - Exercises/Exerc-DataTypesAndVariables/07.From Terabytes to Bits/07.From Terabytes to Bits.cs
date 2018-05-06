492e84a486e06a7fab05ba4912a07d3d43b6616c
anedyalkov <nedyalkov78@gmail.com>
2018-05-06 17:39:57 +0300

    Data Types and Variables - Exercises

---
+++ Data Types and Variables - Exercises/Exerc-DataTypesAndVariables/07.From Terabytes to Bits/07.From Terabytes to Bits.cs
@@ -0,0 +1,13 @@
+﻿namespace _07.From_Terabytes_to_Bits
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            double terrabytes = double.Parse(Console.ReadLine());
+            Console.WriteLine(terrabytes * 1024 * 1024 * 1024 * 1024 * 8);
+        }
+    }
+}
