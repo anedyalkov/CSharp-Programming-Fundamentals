f49178c301dc861167a268fadba3d1878bc843c7
anedyalkov <nedyalkov78@gmail.com>
2018-05-21 14:16:53 +0300

    Array and List Algorithms-Lab

--- Array and List Algorithms-Lab/Lab-Array and List Algorithms/07.Largest N Elements/07.Largest N Elements.cs
+++ Array and List Algorithms-Lab/Lab-Array and List Algorithms/07.Largest N Elements/07.Largest N Elements.cs
@@ -1,15 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _07.Largest_N_Elements
+﻿namespace _07.Largest_N_Elements
 {
-    class Program
+    using System;
+    using System.Linq;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+           var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                var j = i + 1;
+
+                while (j > 0)
+                {
+                    if (numbers[j] > numbers[j - 1])
+                    {
+                        var temp = numbers[j];
+                        numbers[j] = numbers[j - 1];
+                        numbers[j - 1] = temp;
+
+                    }
+
+                    j--;
+                }
+
+            }
+ 
+            var result = numbers.Take(n);
+
+            Console.WriteLine(string.Join(" ", result));
         }
     }
 }
